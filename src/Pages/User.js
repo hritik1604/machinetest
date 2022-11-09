45ef3f7ef518031ceadb9a467472e9abe5c11055
hritik1604 <rutkoli356@gmail.com>
2022-11-09 11:04:32 +0530

    https://hritik1604.github.io/machinetest/

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -1,99 +1,55 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
+import {React , useEffect ,useState } from 'react';
+import {
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Button,
+    NavItem,
+    NavLink,
+    NavbarText,
+  } from 'reactstrap';
 
 const User = () => {
-    const [inpval, setInpval] = useState({
-        name: "",
-        password: "",
-      });
-    
-      const [data, setData] = useState([]);
-      console.log(inpval);
-    
-      const getdata = (e) => {
-        // console.log(e.target.value);
-    
-        const { value, name } = e.target;
-        //  console.log(value,name)
-    
-        setInpval(() => {
-          return {
-            ...inpval,
-            [name]: value,
-          };
-        });
-      };
-    
-      const addData = (e) => {
-        e.preventDefault();
-    
-        const { name , password } = inpval;
-    
-        if (name === "") {
-          alert("name fild is requred");
-        
-        } else if (password === "") {
-          alert("password field is requred");
-        } else if (password.length < 5) {
-          alert("password lenght greater five");
-        } else {
-          console.log("data added succesfully");
-    
-          localStorage.setItem("userdatakey", JSON.stringify([...data, inpval]));
-        }
-      };
-  return (
-   <>
-   <div className="container mt-3">
-        <section>
-          <div className="left_data mt-4">
-            <h3 className="text-center col-lg-4"></h3>
-            <Form>
-              <Form.Group className="mb-3 col-lg-4" controlId="formBasicEmail">
-                <Form.Control
-                  type="text"
-                  name="name"
-                  onChange={getdata}
-                  placeholder="UserName"
-                />
-              </Form.Group>
-              
-              <Form.Group
-                className="mb-3 col-lg-4"
-                controlId="formBasicPassword"
-              >
-                <Form.Control
-                  type="password"
-                  name="password"
-                  onChange={getdata}
-                  placeholder="Password"
-                />
-              </Form.Group>
+    return(
+        <div className="d-flex p-5">
+            <Form  >
+  <FormGroup>
+    <Label for="username">
+      User Name
+    </Label>
+    <Input
+      id="username"
+      name="username"
+      
+      type="text"
+    />
+  </FormGroup>
+  <FormGroup>
+    <Label for="newpassword">
+      New Password
+    </Label>
+    <Input
+      id="newpassword"
+      name="newpassword"
+      
+      type="password"
+    />
+  </FormGroup>
+  <FormGroup className='p-4'>
+  <Button  color="primary" >
+                            Save Password
+                        </Button>
+                        <FormGroup>
+  <Button  color="primary">
+                            Logout
+                        </Button>
+  </FormGroup>
+  </FormGroup>
+  </Form>
+        </div>
+    )
 
-              <Button
-                variant="primary"
-                className="col-lg-1"
-                onClick={addData}
-                type="submit"
-              >
-                Submit
-              </Button>
-            </Form>
-            <p className="mt-3">
-              {/* Already Have an Account */}
-              <span>             
-                <Link to="/UserNewPass">Change Password</Link>
-              </span>
-              
-            </p>
-          </div>
-          <div className="right_data"></div>
-        </section>
-      </div>
-   </>
-  )
 }
 
-export default User
+export default User;
--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,86 +1,68 @@
-import React, { useState } from 'react'
-import TaskList from '../Pages/TaskList';
+import {React , useEffect ,useState } from 'react';
+import { Card,CardBody,CardTitle,CardSubtitle,CardText,Button, Badge } from "reactstrap";
+
 
 const Task = () => {
+    const [users,setUsers] = useState([]);
+    useEffect(()=>{
+        
 
+        console.log("USE EFFECT");
+        fetch('http://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json =>{setUsers(json)})
   
-  const [inputList , setInputList] = useState("");
-  const [Items, setItems] = useState([]);
-
-  const itemEvent = (event) => {
-    setInputList(event.target.value);
-
-  };
-
- 
-
-  const listOfItems = () => {
-    setItems((oldItems) => {
-      return [...oldItems,inputList];
-
-    });
-
-    setInputList("");
-
-  };
-  const deleteItems =(id) =>{
-    console.log("deleted");
   
- 
+  
 
-    setItems((oldItems) => {
-      return oldItems.filter((arrElem, index) =>{
-        return index !==id;
-      });
-    });
-};
-
-
-
-
-
-  return (
-    <>
+    },[]) 
     
-    <div className="container mt-3"></div>
-    <div className="center_data mt-4"></div>
-    <div className="main_div">
-      <div className="center_div">
-        <br/>
-        <div className="center_data mt-4"></div>
-        <h3 className="text-center col-lg-4">Tasks</h3>
-        <br/>
-        
-        <input className="text-left col-lg-4"type="text" placeholder="Enter your Task" value={inputList} onChange={itemEvent}/>
-        
-       
-      
-        
-        
-        
-        
-        <ol>
-          {/* <li>{inputList}</li>  */}
-          {Items.map((itemval, index) =>{
-            return <TaskList key={index}
-            id={index}
-             text={itemval}
-             onSelect={deleteItems}
-             />;
-          } 
-          )}  
-          
-          
-        </ol>
+    return(
         <div>
-        <button onClick={listOfItems}> Add Task </button>
+            
+            {
+              users && users && users.length>0 ? users.map(user => {
+            
+                return (
+                    <div >
+                <Card >
+                    <CardBody className="d-flex p-5 ">
+                        
+                        <CardTitle  tag="p-5">
+                         {user.id}
+                        </CardTitle>
+                        
+                        <CardSubtitle className=" p-5"
+                        >
+                         {user.title}
+                         
+                        </CardSubtitle>
+                        <CardText>
+                         {user.userId}
+                         {user.completed}
+    
+                        </CardText>
+                        <Button color="danger">
+                            Delete
+                        </Button>
+                        
+                    </CardBody>
+
+                </Card>
+                
+                
+                
+                
+                </div>
+                
+                );
+                }):
+        console.log("no display" )
+
+
+            }
         </div>
-
-      </div>
-
-    </div>
-    </>
-  )
+    );
+    
 }
-
 export default Task;
